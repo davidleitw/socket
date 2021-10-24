041709039eb4dd5d2576dbdc919c240dbe9116d5
davidleitw <davidleitw@gmail.com>
2021-10-24 13:35:27 +0800

    fix bug

--- udp_example/udp_client.c
+++ udp_example/udp_client.c
@@ -12,32 +12,37 @@
 int main() 
 {
     char buf[1024] = {0};
-
-    int socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    char recvbuf[1024] = {0};
+    // create an udp socket based on ipv4.
+    int socket_fd = socket(PF_INET, SOCK_DGRAM, 0);
     if (socket_fd < 0) {
         printf("Create socket fail!\n");
         return -1;
     }
 
-    struct sockaddr_in clientSocket = {
-        .sin_family = PF_INET,
+    struct sockaddr_in server = {
+        .sin_family = AF_INET,
         .sin_addr.s_addr = inet_addr(serverIP),
         .sin_port = htons(serverPort)
     };
-    int len = sizeof(clientSocket);
-    
+    int len = sizeof(server);
+
     while (1) {
         printf("Please input your message: ");
         scanf("%s", buf);
 
-        sendto(socket_fd, buf, strlen(buf), 0, (struct sockaddr *)&clientSocket, sizeof(clientSocket));
+        sendto(socket_fd, buf, sizeof(buf), 0, (struct sockaddr *)&server, sizeof(server));
         if (strcmp(buf, "exit") == 0) 
             break;
 
         memset(buf, 0, sizeof(buf));
 
-        recvfrom(socket_fd, buf, strlen(buf), 0, (struct sockaddr *)&clientSocket, &len);
-        printf("get receive message from [%s:%d]: %s\n", inet_ntoa(clientSocket.sin_addr), ntohs(clientSocket.sin_port), buf);
+        if (recvfrom(socket_fd, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&server, &len) < 0) {
+            printf("recvfrom data from %s:%d, failed!", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+        }
+        
+        printf("get receive message from [%s:%d]: %s\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port), recvbuf);
+        memset(recvbuf, 0, sizeof(recvbuf));
     }
 
     if (close(socket_fd) < 0) {
--- udp_example/udp_server.c
+++ udp_example/udp_server.c
@@ -7,44 +7,64 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define serverPort 12000
+
+char *convert(char *src) {
+    char *iter = src;
+    char *result = malloc(sizeof(src));
+    char *it = result;
+    if (iter == NULL) return iter;
+
+    while (*iter) {
+        *it++ = *iter++ & ~0x20;
+    }
+    return result;
+}
+
 int main(int argc , char *argv[])
 {
+    // message buffer
     char buf[1024] = {0};
-    
-    int socket_fd = socket(AF_INET , SOCK_DGRAM , 0);
+
+    int socket_fd = socket(PF_INET , SOCK_DGRAM , 0);
     if (socket_fd < 0){
         printf("Fail to create a socket.");
     }
 
     //socket的連線
     struct sockaddr_in serverAddr = {
-        .sin_family = PF_INET,
+        .sin_family =AF_INET,
         .sin_addr.s_addr = INADDR_ANY,
-        .sin_port = htons(12000)
+        .sin_port = htons(serverPort)
     };
-    struct sockaddr_in clientAddr;
-    int len = sizeof(clientAddr);
-    
     
     if (bind(socket_fd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         perror("Bind socket failed!");
         close(socket_fd);
+        exit(0);
     }
     
     printf("Server ready!\n");
 
+    struct sockaddr_in clientAddr;
+    int len = sizeof(clientAddr);
     while (1) {
         if (recvfrom(socket_fd, buf, sizeof(buf), 0, (struct sockaddr *)&clientAddr, &len) < 0) {
             break;
         }
 
-        printf("get message from [%s:%d]: ", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-        printf("%s\n", buf);
-
         if (strcmp(buf, "exit") == 0) {
+            printf("get exit order, closing the server...\n");
             break;
         }
+        
+        char *conv = convert(buf);
+        printf("get message from [%s:%d]: ", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+        printf("%s -> %s\n", buf, conv);
+
+        sendto(socket_fd, conv, sizeof(conv), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
         memset(buf, 0, sizeof(buf));
+        free(conv);
     }
 
     close(socket_fd);

3a623bf10073e0c5daf37b5914178df2a2b13f23
davidleitw <davidleitw@gmail.com>
2021-10-24 16:15:48 +0800

    新增註解

--- udp_example/udp_client.c
+++ udp_example/udp_client.c
@@ -6,47 +6,58 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define serverPort 12000
+#define serverPort 48763
 #define serverIP "127.0.0.1"
 
 int main() 
 {
+    // message buffer
     char buf[1024] = {0};
     char recvbuf[1024] = {0};
-    // create an udp socket based on ipv4.
+    
+    // 建立 socket
     int socket_fd = socket(PF_INET, SOCK_DGRAM, 0);
     if (socket_fd < 0) {
         printf("Create socket fail!\n");
         return -1;
     }
 
-    struct sockaddr_in server = {
+    // server 地址
+    struct sockaddr_in serverAddr = {
         .sin_family = AF_INET,
         .sin_addr.s_addr = inet_addr(serverIP),
         .sin_port = htons(serverPort)
     };
-    int len = sizeof(server);
+    int len = sizeof(serverAddr);
 
     while (1) {
+        // 輸入資料到 buffer
         printf("Please input your message: ");
         scanf("%s", buf);
 
-        sendto(socket_fd, buf, sizeof(buf), 0, (struct sockaddr *)&server, sizeof(server));
+        // 傳送到 server 端
+        sendto(socket_fd, buf, sizeof(buf), 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+        
+        // 接收到 exit 指令就退出迴圈
         if (strcmp(buf, "exit") == 0) 
             break;
 
+        // 清空 message buffer
         memset(buf, 0, sizeof(buf));
 
-        if (recvfrom(socket_fd, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&server, &len) < 0) {
-            printf("recvfrom data from %s:%d, failed!", inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+        // 等待 server 回傳轉成大寫的資料
+        if (recvfrom(socket_fd, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&serverAddr, &len) < 0) {
+            printf("recvfrom data from %s:%d, failed!", inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port));
         }
         
-        printf("get receive message from [%s:%d]: %s\n", inet_ntoa(server.sin_addr), ntohs(server.sin_port), recvbuf);
+        // 顯示 server 地址，以及收到的資料
+        printf("get receive message from [%s:%d]: %s\n", inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port), recvbuf);
         memset(recvbuf, 0, sizeof(recvbuf));
     }
-
+    // 關閉 socket，並檢查是否關閉成功
     if (close(socket_fd) < 0) {
         perror("close socket failed!");
     }
+
     return 0;
 }
--- udp_example/udp_server.c
+++ udp_example/udp_server.c
@@ -7,7 +7,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define serverPort 12000
+#define serverPort 48763
 
 char *convert(char *src) {
     char *iter = src;
@@ -26,18 +26,20 @@ int main(int argc , char *argv[])
     // message buffer
     char buf[1024] = {0};
 
+    // 建立 socket
     int socket_fd = socket(PF_INET , SOCK_DGRAM , 0);
     if (socket_fd < 0){
         printf("Fail to create a socket.");
     }
-
-    //socket的連線
+    
+    // server 地址
     struct sockaddr_in serverAddr = {
         .sin_family =AF_INET,
         .sin_addr.s_addr = INADDR_ANY,
         .sin_port = htons(serverPort)
     };
     
+    // 將建立的 socket 綁定到 serverAddr 指定的 port
     if (bind(socket_fd, (const struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
         perror("Bind socket failed!");
         close(socket_fd);
@@ -49,24 +51,36 @@ int main(int argc , char *argv[])
     struct sockaddr_in clientAddr;
     int len = sizeof(clientAddr);
     while (1) {
+        // 當有人使用 UDP 協定送資料到 48763 port
+        // 會觸發 recvfrom()，並且把來源資料寫入 clientAddr 當中
         if (recvfrom(socket_fd, buf, sizeof(buf), 0, (struct sockaddr *)&clientAddr, &len) < 0) {
             break;
         }
 
+        // 收到 exit 指令就關閉 server
         if (strcmp(buf, "exit") == 0) {
             printf("get exit order, closing the server...\n");
             break;
         }
         
+        // 將收到的英文字母換成大寫
         char *conv = convert(buf);
+
+        // 顯示資料來源，原本資料，以及修改後的資料
         printf("get message from [%s:%d]: ", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
         printf("%s -> %s\n", buf, conv);
 
+        // 根據 clientAddr 的資訊，回傳至 client 端                
         sendto(socket_fd, conv, sizeof(conv), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+
+        // 清空 message buffer
         memset(buf, 0, sizeof(buf));
         free(conv);
     }
-
-    close(socket_fd);
+    // 關閉 socket，並檢查是否關閉成功
+    if (close(socket_fd) < 0) {
+        perror("close socket failed!");
+    }
+    
     return 0;
 }

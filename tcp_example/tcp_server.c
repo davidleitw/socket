7579378d0b80122be280297d803665ee9991b8bb
N0Ball <n0ball@gmail.com>
2021-11-18 14:55:08 +0800

    Add a tcp connection for multi-user with multithread

--- tcp_example/tcp_server.c
+++ tcp_example/tcp_server.c
@@ -2,11 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
 #define serverPort 48763
+#define BACKLOG 5
+
+pthread_t clients[BACKLOG];
+pthread_t tid;
 
 char *convert(char *src) {
     char *iter = src;
@@ -20,10 +25,57 @@ char *convert(char *src) {
     return result;
 }
 
+struct client_info{
+    int sockfd;
+    struct sockaddr_in clientAddr;
+};
+
+void* clientSocket(void *param){
+    char buf[1024];
+    struct client_info * info = param;
+    // 不斷接收 client 資料
+    while (recv(info->sockfd, buf, sizeof(buf), 0)) {
+        // 收到 exit 指令就離開迴圈
+        if (strcmp(buf, "exit") == 0) {
+            memset(buf, 0, sizeof(buf));
+            break;
+        }
+
+        // 將收到的英文字母換成大寫
+        char *conv = convert(buf);
+
+        // 顯示資料來源，原本資料，以及修改後的資料
+        printf("get message from [%s:%d]: ",
+                inet_ntoa(info->clientAddr.sin_addr), ntohs(info->clientAddr.sin_port));
+        printf("%s -> %s\n", buf, conv);
+        
+        // 傳回 client 端
+        // 不需要填入 client 端的位置資訊，因為已經建立 TCP 連線
+        if (send(info->sockfd, conv, sizeof(conv), 0) < 0) {
+            printf("send data to %s:%d, failed!\n", 
+                    inet_ntoa(info->clientAddr.sin_addr), ntohs(info->clientAddr.sin_port));
+            memset(buf, 0, sizeof(buf));
+            free(conv);
+            break;
+        }
+
+        // 清空 message buffer
+        memset(buf, 0, sizeof(buf));
+        free(conv);
+    }
+
+    // 關閉 reply socket，並檢查是否關閉成功
+    if (close(info->sockfd) < 0) {
+        perror("close socket failed!");
+    }
+}
+
 int main() 
 {
     // message buffer
     char buf[1024] = {0};
+
+    int client_index = 0;
 
     // 建立 socket
     int socket_fd = socket(PF_INET , SOCK_STREAM , 0);
@@ -48,7 +100,7 @@ int main()
     // 初始化，準備接受 connect
     // backlog = 5，在 server accept 動作之前，最多允許五筆連線申請
     // 回傳 -1 代表 listen 發生錯誤
-    if (listen(socket_fd, 5) == -1) {
+    if (listen(socket_fd, BACKLOG) == -1) {
         printf("socket %d listen failed!\n", socket_fd);
         close(socket_fd);
         exit(0);
@@ -58,6 +110,8 @@ int main()
             inet_ntoa(serverAddr.sin_addr), ntohs(serverAddr.sin_port));
 
     while(1) {
+
+        // 建立新socket的參數
         int reply_sockfd;
         struct sockaddr_in clientAddr;
         int client_len = sizeof(clientAddr);
@@ -67,45 +121,18 @@ int main()
         reply_sockfd = accept(socket_fd, (struct sockaddr *)&clientAddr, &client_len);
         printf("Accept connect request from [%s:%d]\n", 
                 inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-        
-        // 不斷接收 client 資料
-        while (recv(reply_sockfd, buf, sizeof(buf), 0)) {
-            // 收到 exit 指令就離開迴圈
-            if (strcmp(buf, "exit") == 0) {
-                memset(buf, 0, sizeof(buf));
-                goto exit;
-            }
 
-            // 將收到的英文字母換成大寫
-            char *conv = convert(buf);
+        // 建立 thread 與 thread 的參數
+        struct client_info *newClientinfo = malloc(sizeof(struct client_info));
+        memcpy(&newClientinfo->sockfd, &reply_sockfd, sizeof(int));
+        memcpy(&newClientinfo->clientAddr, &clientAddr, sizeof(struct sockaddr_in));
 
-            // 顯示資料來源，原本資料，以及修改後的資料
-            printf("get message from [%s:%d]: ",
-                    inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-            printf("%s -> %s\n", buf, conv);
-            
-            // 傳回 client 端
-            // 不需要填入 client 端的位置資訊，因為已經建立 TCP 連線
-            if (send(reply_sockfd, conv, sizeof(conv), 0) < 0) {
-                printf("send data to %s:%d, failed!\n", 
-                        inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-                memset(buf, 0, sizeof(buf));
-                free(conv);
-                goto exit;
-            }
-
-            // 清空 message buffer
-            memset(buf, 0, sizeof(buf));
-            free(conv);
+        if (pthread_create(&clients[client_index++], NULL, clientSocket, newClientinfo) != 0){
+            printf("Failed to create new Thread!");
         }
-        
-        // 關閉 reply socket，並檢查是否關閉成功
-        if (close(reply_sockfd) < 0) {
-            perror("close socket failed!");
-        }
+
     }
 
-exit:
     // 關閉 socket，並檢查是否關閉成功
     if (close(socket_fd) < 0) {
         perror("close socket failed!");
